08da4be6342deff6f875ec39ef22ec405e223b30
Jeferson Mejicanos <alexandermejicanos57@gmail.com>
2023-07-17 23:02:18 -0600

    Implement the mobil first design

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let navbar = document.querySelector('#navbar');
 hamburger.addEventListener('click', function() {
     navbar.classList.toggle('open');
 });
+
+hamburger.addEventListener('click', function() {
+    // Toggle la clase "open" en el elemento de hamburguesa
+    this.classList.toggle('open');
+});
