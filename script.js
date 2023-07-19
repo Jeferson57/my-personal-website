2dd8c0b9d0cb9b4097c1ab44e749000b69136b1a
Jeferson Mejicanos <alexandermejicanos57@gmail.com>
2023-07-18 23:42:11 -0600

    Add ease-in-out animation to the icon menu

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let hamburger = document.querySelector('.hamburger');
 let navbar = document.querySelector('#navbar');
 
-hamburger.addEventListener('click', function() {
-    navbar.classList.toggle('open');
+
+// hamburger.addEventListener('click', function() {
+//     // navbar.classList.toggle('open');
+
+//     // Toggle la clase "open" en el elemento de hamburguesa
+// });
+
+let iconMenu = document.querySelector('.icon-menu');
+let iconClose = document.querySelector('.icon-close');
+
+iconMenu.addEventListener('click', function() {
+  let navbar = document.querySelector('#navbar');
+  navbar.classList.add('open');
+  hamburger.classList.toggle('open');
 });
 
-hamburger.addEventListener('click', function() {
-    // Toggle la clase "open" en el elemento de hamburguesa
-    this.classList.toggle('open');
+iconClose.addEventListener('click', function() {
+  let navbar = document.querySelector('#navbar');
+  navbar.classList.remove('open');
+  hamburger.classList.toggle('open');
 });
